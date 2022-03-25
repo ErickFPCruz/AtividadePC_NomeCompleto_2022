6d5f247f40f70b00274917a23e42335891166654
ErickFPCruz <erick.cruz97@hotmail.com>
2022-03-25 20:18:36 -0300

    Nomeação Concluída

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome;
+
+Console.Write("Escreva seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Escreva seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine("");
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de Catálogo: {sobrenome.ToUpper()}, {nome}");
